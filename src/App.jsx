0415e103ca96da97c8b897c2b16df1e4bade19cf
mjhervasmozos26 <mjhervasmozos26@gmail.com>
2026-06-16 09:32:49 +0200

    primera subida

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+     <div className="container">
+      <h1>María José Hervás Mozos</h1>
+
+
+
+     </div>
+    </>
+  )
+}
+
+export default App
